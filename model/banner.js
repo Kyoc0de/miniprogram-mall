c06b39953ec2a77a96f2ff8f7f41c4442b0e9c8e
kyoo0 <kyoscode@gmail.com>
2020-03-18 22:00:36 -0400

    banner部分显示

--- model/banner.js
+++ model/banner.js
@@ -1,5 +1,14 @@
-class Banner {
-    static getHomeLocationB(){
+import {Http} from "../utils/http";
 
+class Banner {
+    static locationB = 'b-1'
+    static async getHomeLocationB(){
+        return await Http.request({
+            url:`banner/name/${Banner.locationB}`
+        })
     }
+}
+
+export {
+    Banner
 }
--- pages/home/home.js
+++ pages/home/home.js
@@ -1,5 +1,6 @@
 // pages/home/home.js
 import {Theme} from "../../model/theme";
+import {Banner} from "../../model/banner";
 
 Page({
 
@@ -7,18 +8,27 @@ Page({
    * 页面的初始数据
    */
   data: {
-    topTheme:null,
+    themeA:null,
+    bannerB:null,
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: async function (options) {
-    const data  = await Theme.getHomeLocationA()
-    this.setData({
-      topTheme: data[0]
-    })
 
+    this.initAllData()
+
+  },
+
+  async initAllData(){
+    const themeA  = await Theme.getHomeLocationA()
+    const bannerB = await Banner.getHomeLocationB()
+
+    this.setData({
+      themeA: themeA[0],
+      bannerB
+    })
   },
 
   /**
